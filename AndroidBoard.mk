35fdc14ca0ec83eefda62513e7b5a618c09aa589
Simon1511 <me@simon1511.de>
2023-10-10 17:31:56 +0200

    Initial a73xq tree
    Change-Id: I77e2efb8a2b5e0944e15219d3e9adc77d27b89f4

---
+++ AndroidBoard.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+INSTALLED_KERNEL_TARGET := $(PRODUCT_OUT)/kernel
+recovery_uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk-recovery.cpio
+
+RECOVERY_KMOD_TARGETS := \
+    sec_cmd.ko \
+    sec_common_fn.ko \
+    sec_secure_touch.ko \
+    sec_tclm_v2.ko \
+    sec_tsp_dumpkey.ko \
+    sec_tsp_log.ko \
+    synaptics_ts.ko
+
+RECOVERY_FIRMWARE_TARGETS := \
+    s3908_a73xq_boe.bin \
+    s3908_a73xq_csot.bin \
+    s3908_a73xq_sdc_4th.bin \
+    s3908_a73xq_sdc.bin
+
+INSTALLED_RECOVERY_KMOD_TARGETS := $(RECOVERY_KMOD_TARGETS:%=$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/%)
+$(INSTALLED_RECOVERY_KMOD_TARGETS): $(INSTALLED_KERNEL_TARGET)
+	echo -e ${CL_GRN}"Copying kernel modules to recovery"${CL_RST}
+	@mkdir -p $(dir $@)
+	cp $(@F:%=$(TARGET_OUT_VENDOR)/lib/modules/%) $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/
+
+INSTALLED_RECOVERY_FIRMWARE_TARGETS := $(RECOVERY_FIRMWARE_TARGETS:%=$(TARGET_RECOVERY_ROOT_OUT)/vendor/firmware/tsp_synaptics/%)
+$(INSTALLED_RECOVERY_FIRMWARE_TARGETS): $(INSTALLED_KERNEL_TARGET)
+	echo -e ${CL_GRN}"Copying touchscreen firmware to recovery"${CL_RST}
+	@mkdir -p $(dir $@)
+	cp $(@F:%=$(TARGET_OUT_VENDOR)/firmware/tsp_synaptics/%) $(TARGET_RECOVERY_ROOT_OUT)/vendor/firmware/tsp_synaptics/
+
+$(recovery_uncompressed_ramdisk): $(INSTALLED_RECOVERY_KMOD_TARGETS) $(INSTALLED_RECOVERY_FIRMWARE_TARGETS)
---
+++ BoardConfig.mk
@@ -0,0 +1,48 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/samsung/a73xq
+
+include device/samsung/sm7325-common/BoardConfigCommon.mk
+
+# Kernel
+TARGET_KERNEL_CONFIG        := vendor/lineage-a73xq_defconfig
+BOARD_NAME                  := SRPUH27A001
+
+# Kernel modules
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
+
+# Recovery
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+
+# Display
+TARGET_SCREEN_DENSITY := 450
+
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := a73xq
+
+# Security patch
+VENDOR_SECURITY_PATCH := 2023-08-01
+
+# UDFPS
+TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.a73xq
+TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x2000U | 0x400000000LL
+SOONG_CONFIG_qtidisplay_udfps := true
+TARGET_SEC_FP_REQUEST_FORCE_CALIBRATE := true
+TARGET_SEC_FP_REQUEST_TOUCH_EVENT := true
+
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
