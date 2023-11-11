20dbf2e2e44a6397579af9145574298bde797ad0
Simon1511 <me@simon1511.de>
2023-11-11 13:46:16 +0100

    a73xq: PE bringup
    Change-Id: I6561c414eb51e1fd2b32e414d68b60ab102dd0b0

--- lineage_a73xq.mk
+++ lineage_a73xq.mk
@@ -23,8 +23,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
@@ -34,7 +34,7 @@ TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_a73xq
+PRODUCT_NAME := aosp_a73xq
 PRODUCT_DEVICE := a73xq
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A736B
