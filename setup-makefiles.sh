ed31dcb24944d9d8290fe9dbdd0a27807bbbbb33
Simon1511 <me@simon1511.de>
2024-03-17 12:50:37 +0100

    a73xq: call correct script in setup-makefiles.sh
    Change-Id: I228a216e691e6170dda6f10a03ff40adbf75f9d3

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -17,4 +17,4 @@ export DEVICE=a73xq
 export DEVICE_COMMON=sm7325-common
 export VENDOR=samsung
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
