8cf83afdb31ca3425ae0881eb1ce4b25d2266e3b
Bruno Martins <bgcngm@gmail.com>
2025-04-30 00:00:08 +0100

    a73xq: Set UDFPS extension lib via soong config
    Change-Id: I8a709bde4843b61420ea1003a749952d2619e2da

--- device.mk
+++ device.mk
@@ -44,3 +44,4 @@ PRODUCT_PACKAGES += \
 
 # UDFPS
 $(call soong_config_set,samsung_udfps,udfps_zorder,0x20000000u)
+$(call soong_config_set,surfaceflinger,udfps_lib,//hardware/samsung/fingerprint:libudfps_extension.samsung)
